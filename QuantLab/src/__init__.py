a2eb8c649594959607af0cfb3c5de45485ee41d1
BANOTH VIKAS <vicky.b1902@gmail.com>
2026-07-23 22:46:42 +0530

    Delete QuantLab directory

--- QuantLab/src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-QuantLab Quantitative Library.
-Provides high-performance market data retrieval, technical indicators, financial metrics, backtesting, and portfolio analytics.
-"""
-
-__version__ = "1.0.0"
-__author__ = "QuantLab Core Devs"
